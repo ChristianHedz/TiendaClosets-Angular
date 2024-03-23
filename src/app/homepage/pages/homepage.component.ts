b7fb5d85b308488e665cccfe4f0d165f6a546656
Chris <cristian.28hedz@gmail.com>
2024-03-23 17:02:43 -0600

    style: 💄Agregue pagina w de home con componentes de nav,  carrousel y cards

---
+++ src/app/homepage/pages/homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomePageComponent  {
+
+}
