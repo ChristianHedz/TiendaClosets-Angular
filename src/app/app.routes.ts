293670594fefa1fd21221c5e523ba5d065f346f7
Chris <cristian.28hedz@gmail.com>
2024-03-28 13:08:12 -0600

    feat: ✨ Agregue infoComponent para la pagina de informacion y le agregue el card3dcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,16 @@ import { BarChartComponent } from './dashboard/components/bar-chart/bar-chart.co
 import { LineChartComponent } from './dashboard/components/line-chart/line-chart.component';
 import { HomePageComponent } from './homepage/pages/homepage/homepage.component';
 import { ProductsComponent } from './homepage/pages/products/products.component';
+import { Card3DComponent} from './components/carrousels/carrousel-bootstrap/card-3d.component';
+import { InfoComponent } from './homepage/pages/info/info.component';
 
 export const routes: Routes = [
   {path: '',redirectTo: 'dashboard',pathMatch: 'full'},
-  {path: 'dashboard',component: DashboardComponent},
+  {path: 'dashboard',component: Card3DComponent},
   {path: 'login',component: LoginComponent, canActivate: [isNotAuthGuard]},
   {path: 'bar',component: BarChartComponent},
   {path: 'line',component: LineChartComponent},
   {path: 'home',component: HomePageComponent},
-  {path: 'closets',component: ProductsComponent}
+  {path: 'closets',component: ProductsComponent},
+  {path: 'info',component: InfoComponent}
 ];
